581e099bad1e6f2369be03edac4ab90dcdc216a8
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-07 09:44:38 +0300

    Implement number click handler

--- src/buttons/numbers.js
+++ src/buttons/numbers.js
@@ -1,12 +1,29 @@
-import { populateScreen } from "../screen/populateScreen";
-import { calculator } from "../calculator/calculator";
+import { updateScreen } from "../screen/updateScreen";
+import { Calculator, calculator } from "../calculator/calculator";
+import "../styles/numbers.scss";
 
-const numbers = document.querySelectorAll(".number");
-for (let number of numbers) {
-  number.addEventListener("click", setCalculatorValue);
-  number.addEventListener("click", populateScreen);
+class NumberButtonCommand extends Calculator {
+  constructor(numberButton) {
+    super();
+    this.numberButton = numberButton;
+  }
+
+  execute(value) {
+    return +(value + this.numberButton); // in order to remove initial 0
+  }
+
+  undo(value) {
+    return value.toString().substring(0, value.toString().length - 1);
+  }
 }
 
-function setCalculatorValue(event) {
-  calculator.value = calculator.value + event.target.textContent;
+const numbers = document.querySelectorAll(".number");
+
+for (let number of numbers) {
+  number.addEventListener("click", numberCommand);
+}
+
+function numberCommand(event) {
+  calculator.execute(new NumberButtonCommand(event.target.textContent));
+  updateScreen();
 }
