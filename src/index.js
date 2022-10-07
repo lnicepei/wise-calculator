0f3563ec677790deafeea787e2e0aef56f23a827
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-07 09:42:04 +0300

    Leave only needed imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import "./styles/background.scss";
 import "./styles/rewrite.scss";
-import "./styles/numbers.scss";
-import "./styles/screen.scss";
-import "./styles/calculator.scss";
-import { calculator } from "./calculator/calculator.js";
-import { populateScreen } from "./screen/populateScreen";
 import "./buttons/operations";
 import "./buttons/numbers";
-import { screen } from "./screen/screen";
-
-populateScreen(calculator.value);
+import "./screen/updateScreen";
