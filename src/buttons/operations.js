82849d1bd3a84359eb0ae6b82aefb37f33799118
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-07 09:45:00 +0300

    Move operations here

--- src/buttons/operations.js
+++ src/buttons/operations.js
@@ -1,34 +1,111 @@
-import { calculator } from "../calculator/calculator";
-import {
-  AddCommand,
-  DivideCommand,
-  MultiplyCommand,
-  SubtractCommand,
-} from "../operations/operations";
-import { screen } from "../screen/screen";
-import "../styles/numbers.scss";
+import { calculator, Calculator } from "../calculator/calculator";
+import { updateScreen } from "../screen/updateScreen";
 
-const operations = document.querySelector(".operations").children;
-console.log(operations);
-for (let element of operations) {
-  element.addEventListener("click", eventHandler);
+export class AddCommand extends Calculator {
+  constructor(addOperand) {
+    super();
+    this.addOperand = +addOperand;
+  }
+
+  execute(value) {
+    return +(value + this.addOperand);
+  }
+
+  undo(value) {
+    return +value - this.addOperand;
+  }
 }
 
-function eventHandler(event) {
+export class MultiplyCommand extends Calculator {
+  constructor(multiplyOperand) {
+    super();
+    this.multiplyOperand = +multiplyOperand;
+  }
+
+  execute(value) {
+    return +(value * this.multiplyOperand);
+  }
+
+  undo(value) {
+    return +value / this.multiplyOperand;
+  }
+}
+
+export class DivideCommand extends Calculator {
+  constructor(divideOperand) {
+    super();
+    this.divideOperand = +divideOperand;
+  }
+
+  execute(value) {
+    return +value / this.divideOperand;
+  }
+
+  undo(value) {
+    return +value * this.divideOperand;
+  }
+}
+
+export class SubtractCommand extends Calculator {
+  constructor(subtractOperand) {
+    super();
+    this.subtractOperand = +subtractOperand;
+  }
+
+  execute(value) {
+    return +value - this.subtractOperand;
+  }
+
+  undo(value) {
+    return +value + this.subtractOperand;
+  }
+}
+
+export class AllClearCommand extends Calculator {
+  constructor() {
+    super();
+  }
+
+  execute() {
+    calculator.operations = [];
+    return 0;
+  }
+
+  undo(value) {
+    return +value;
+  }
+}
+
+const operations = document.querySelectorAll(
+  ".simple-operations, .advanced-operations"
+);
+
+for (let element of operations) {
+  element.addEventListener("click", arithmeticCommandSelector);
+}
+
+export function arithmeticCommandSelector(event) {
   switch (event.target.textContent) {
     case "+":
-      calculator.execute(new AddCommand(screen.rightOperand));
+      calculator.execute(new AddCommand(calculator.value));
       break;
     case "-":
-      calculator.execute(new SubtractCommand(screen.rightOperand));
+      calculator.execute(new SubtractCommand(calculator.value));
       break;
     case "*":
-      calculator.execute(new MultiplyCommand(screen.rightOperand));
+      calculator.execute(new MultiplyCommand(calculator.value));
       break;
     case "/":
-      calculator.execute(new DivideCommand(screen.rightOperand));
+      calculator.execute(new DivideCommand(calculator.value));
       break;
-
+    case "AC":
+      calculator.execute(new AllClearCommand());
+      updateScreen();
+      break;
+    case "undo":
+      calculator.undo();
+      updateScreen();
+      break;
     default:
       break;
   }
