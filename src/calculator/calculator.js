3f7bae5b35d9729960357907d0939355b94fffd0
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-07 09:45:28 +0300

    Try to add another operand

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -1,19 +1,24 @@
 import "../styles/calculator.scss";
+import "../styles/background.scss";
 
 export class Calculator {
-  constructor(value = 0) {
-    this.value = value;
+  constructor() {
+    // this.firstOperand = 0;
+    // this.secondOperand = null; //TODO: what to do with those operands??
+    this.value = 0;
+    this.previousValue = null;
     this.operations = [];
   }
 
   execute(command) {
-    this.operations.push(command);
+    if (this.operations[this.operations.length - 1] !== command)
+      this.operations.push(command);
     this.value = command.execute(this.value);
   }
 
   undo() {
     const lastCommand = this.operations.pop();
-    this.value = lastCommand.undo(this.value);
+    this.value = lastCommand.undo(this.value) || 0; // TODO: undo all operations should return this.value = 0
   }
 }
 
