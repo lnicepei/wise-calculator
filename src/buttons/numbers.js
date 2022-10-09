5649530ef4a35ad9c56c95cb12073785f74f3302
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-09 16:36:07 +0300

    Allow adding dot

--- src/buttons/numbers.js
+++ src/buttons/numbers.js
@@ -1,54 +1,53 @@
 import { updateScreen } from "../screen/updateScreen";
 import { Calculator, calculator } from "../calculator/calculator";
 import "../styles/numbers.scss";
+// Хранить значение в виде строки (для удобства добавления .)
 
-class NumberButtonCommand extends Calculator {
+export class NumberButtonCommand extends Calculator {
   constructor(numberButton) {
     super();
     this.numberButton = numberButton;
   }
 
   execute(value, previousValue, operations) {
-    console.log(operations);
-    if (value > 1000000000000000) value = 0;
+    if (value > 1000000000000000 || previousValue > 1000000000000000)
+      return [value, previousValue, operations];
 
     if (this.numberButton === ".") {
       if (value !== null && !value.toString().includes(".")) {
-        return [value + ".", previousValue];
+        return [value + ".", previousValue, operations];
       }
-      if(!previousValue.toString().includes(".")) {
-        return [value, previousValue + "."];
+      if (!previousValue.toString().includes(".")) {
+        return [value, previousValue + ".", operations];
       }
+      return [value, previousValue, operations];
     }
 
     if (previousValue === null && value === null) {
-      return [value, +this.numberButton.toString()];
+      return [value, +this.numberButton, operations];
     } else if (value === null) {
-      return [
-        value,
-        +(previousValue.toString() + this.numberButton.toString()),
-      ];
+      return [value, +(previousValue + this.numberButton), operations];
     } else {
-      return [
-        +(value.toString() + this.numberButton.toString()),
-        previousValue,
-      ];
+      return [+(value + this.numberButton), previousValue, operations];
     }
   }
 
-  undo(value, previousValue) {
-    console.log("Undo from numbers");
-    if (value === null) {
+  undo(value, previousValue, operations) {
+    if (previousValue === null && value === null) {
+      return [value, previousValue, operations];
+    } else if (value === null || value === 0) {
       return [
         null,
         +previousValue
-          ?.toString()
+          .toString()
           .substring(0, previousValue.toString().length - 1) || null,
-      ]; // FIXME: undo no longer works
+        operations,
+      ];
     } else {
       return [
         +value.toString().substring(0, value.toString().length - 1) || null,
         previousValue,
+        operations,
       ];
     }
   }
