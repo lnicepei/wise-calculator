cdd8e884c3e34a8a2baeac27f6775730ca1a6c52
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-09 16:28:16 +0300

    Make operations extend Calculator

--- src/buttons/operations.js
+++ src/buttons/operations.js
@@ -1,54 +1,85 @@
 import { calculator, Calculator } from "../calculator/calculator";
 import { updateScreen } from "../screen/updateScreen";
+import { NumberButtonCommand } from "./numbers";
 
 export class AddCommand extends Calculator {
-  constructor(addOperand) { // addOperand == calculator.value
+  constructor(value, previousValue) {
+    // encapsulated values of  calculator.value и calculator.previousValue
     super();
-    this.addOperand = addOperand;
+    this.value = value;
+    this.previousValue = previousValue;
+    this.operands = [value, previousValue];
   }
-
-  execute(value, previousValue) {
-    if (value === null) {
-      return [0, previousValue];
+// should I pass any arguments here???????
+  execute(value, previousValue, operations) {
+    // if (previousValue === null && value === null) {
+    //   return [value, previousValue, operations];
+    // } else if (value === null) {
+    //   return [0, previousValue, operations];
+    // } else {
+    //   updateScreen(previousValue + this.value);
+    //   return [0, previousValue + this.value, operations];
+    // }
+    if (previousValue === null && value === null) {
+      return [value, previousValue, operations];
+    } else if (value === null) {
+      return [0, this.operands[1], operations];
     } else {
-      updateScreen(previousValue + this.addOperand);
-      return [0, previousValue + this.addOperand];
+      updateScreen(previousValue + this.value);
+      return [0, this.operands[0] + this.operands[1], operations];
     }
   }
 
-  undo(value, previousValue) {
-    console.log("Undo from operations");
+  undo(value, previousValue, operations) {
     if (value === 0 || value === null) {
-      return [null, previousValue - this.addOperand];
+      return [null, previousValue - this.value, operations];
     } else {
-      return [value, previousValue];
+      return [0, value ?? previousValue - this.value, operations];
     }
   }
 }
 
 export class MultiplyCommand extends Calculator {
-  constructor(multiplyOperand) {
+  constructor(value, previousValue) {
     super();
-    this.multiplyOperand = +multiplyOperand;
+    this.value = value;
+    this.previousValue = previousValue;
+    this.operands = [value, previousValue]
   }
 
-  execute(value, previousValue) {
-    if (value === null) {
-      return [0, previousValue];
+  execute(value, previousValue, operations) {
+    // if (previousValue === null && value === null) {
+    //   return [value, previousValue, operations];
+    // } else if (value === null) {
+    //   return [0, previousValue, operations];
+    // } else {
+    //   updateScreen(previousValue * this.value);
+    //   return [0, previousValue * this.value, operations];
+    // }
+    // return [0, value + this.value, operations];
+    if (previousValue === null && value === null) {
+      return [value, previousValue, operations];
+    } else if (value === null) {
+      return [0, this.operands[1], operations];
     } else {
-      updateScreen(previousValue * this.multiplyOperand);
-      return [0, previousValue * this.multiplyOperand];
+      updateScreen(previousValue + this.value);
+      return [0, this.operands[0] + this.operands[1], operations];
     }
   }
 
-  undo(value) {
-    return +value / this.multiplyOperand;
+  undo(value, previousValue, operations) {
+    if (value === 0 || value === null) {
+      return [previousValue, previousValue / this.value, operations];
+    } else {
+      console.log(previousValue, this.value);
+      return [0, value ?? previousValue / this.value, operations];
+    }
   }
 }
 
-export class DivideCommand extends Calculator {
+export class DivideCommand {
   constructor(divideOperand) {
-    super();
+    // super();
     this.divideOperand = +divideOperand;
   }
 
@@ -62,9 +93,9 @@ export class DivideCommand extends Calculator {
   }
 }
 
-export class SubtractCommand extends Calculator {
+export class SubtractCommand {
   constructor(subtractOperand) {
-    super();
+    // super();
     this.subtractOperand = +subtractOperand;
   }
 
@@ -79,19 +110,19 @@ export class SubtractCommand extends Calculator {
 }
 
 export class AllClearCommand extends Calculator {
-  constructor() {
+  constructor(value, previousValue, operations) {
     super();
+    this.value = value;
+    this.previousValue = previousValue;
+    this.operations = operations;
   }
 
   execute() {
-    calculator.previousValue = 0;
-    calculator.value = null;
-    calculator.operations = [];
-    return 0; // FIXME: return proper resetters
+    return [null, null, []];
   }
 
-  undo(value) {
-    return +value;
+  undo() {
+    return [this.value, this.previousValue, this.operations];
   }
 }
 
@@ -104,21 +135,32 @@ for (let element of operations) {
 }
 
 export function arithmeticCommandSelector(event) {
-  switch (event.target.textContent) {
+  // TODO: Выполнять операцию только тогда, когда есть два операнда??????Г а так: проверять это условие в нажимальщике кнопок
+  switch (event?.target?.textContent || event) {
     case "+":
-      calculator.execute(new AddCommand(calculator.value));
+      calculator.execute(
+        new AddCommand(+calculator.value, +calculator.previousValue)
+      );
       break;
     case "-":
       calculator.execute(new SubtractCommand(calculator.value));
       break;
     case "*":
-      calculator.execute(new MultiplyCommand(calculator.value));
+      calculator.execute(
+        new MultiplyCommand(+calculator.value, +calculator.previousValue)
+      );
       break;
     case "/":
       calculator.execute(new DivideCommand(calculator.value));
       break;
     case "AC":
-      calculator.execute(new AllClearCommand());
+      calculator.execute(
+        new AllClearCommand(
+          calculator.value,
+          calculator.previousValue,
+          calculator.operations
+        )
+      );
       updateScreen();
       break;
     case "undo":
