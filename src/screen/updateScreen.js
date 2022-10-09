3d83c1b34c257b32e438accfe0302f6a3f651ec4
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-09 16:37:29 +0300

    Prettier code

--- src/screen/updateScreen.js
+++ src/screen/updateScreen.js
@@ -3,9 +3,8 @@ import "../styles/screen.scss";
 
 export function updateScreen(value) {
   const screen = document.querySelector(".screen");
-  screen.textContent = value || `${
-    calculator.value ?? calculator.previousValue ?? 0
-  }`;
+  screen.textContent =
+    value || (calculator.value ?? calculator.previousValue ?? 0);
 }
 
 updateScreen();
