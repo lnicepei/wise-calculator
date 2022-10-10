f02496568904d8d550bdf5df68ecc50851c0684d
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-10 20:47:38 +0300

    Handle 0 result behavior

--- src/screen/updateScreen.js
+++ src/screen/updateScreen.js
@@ -3,8 +3,8 @@ import "../styles/screen.scss";
 
 export function updateScreen(value) {
   const screen = document.querySelector(".screen");
+  if (value === 0) value = "0";
   screen.textContent =
     value || (calculator.value ?? calculator.previousValue ?? 0);
 }
-
 updateScreen();
