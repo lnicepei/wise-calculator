50255b119b331538f5f31aa28c26f51a0ea0e498
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-11 21:08:39 +0300

    Remove calculator.value from display

--- src/screen/updateScreen.js
+++ src/screen/updateScreen.js
@@ -5,7 +5,7 @@ export function updateScreen(value) {
   const screen = document.querySelector(".screen");
   if (value === 0) value = "0";
   screen.textContent =
-    value || (calculator.value ?? calculator.previousValue ?? 0);
+    value || (calculator.previousValue ?? 0);
 }
 
 updateScreen();
