1397ea8de7cb10230d32dc4113941d811d49fb25
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-15 14:53:55 +0300

    Remove old code

--- src/operations/advancedOperations/PowerOfYCommand.js
+++ src/operations/advancedOperations/PowerOfYCommand.js
@@ -1,6 +1,5 @@
 import { calculator, Calculator } from "../../calculator/calculator";
 import { updateScreen } from "../../screen/updateScreen";
-import { arithmeticCommandSelector } from "../../selectors/arithmeticOperationSelector";
 
 export class PowerOfYCommand extends Calculator {
   constructor() {
@@ -9,20 +8,10 @@ export class PowerOfYCommand extends Calculator {
     this.previousValue = calculator.previousValue;
   }
 
-  executeFirstOperation() {}
-
-  // executeWithOneArg() {
-  //   calculator.value = 0;
-  //   updateScreen();
-  // }
-
-  // executeWithTwoArgs() {
-  //   if (calculator.operationSigns.at(-1) !== "x^y")
-  //     arithmeticCommandSelector(calculator.operationSigns.at(-1));
-  // calculator.previousValue = calculator.previousValue ** calculator.value;
-  // calculator.value = 0;
-  //   updateScreen();
-  // }
+  executeFirstOperation() {
+    calculator.value = 0;
+    updateScreen();
+  }
 
   executeWithOneArg() {
     calculator.value = 0;
