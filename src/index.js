c66e3f899de9abc9d7bbe660b718910e7693c6d1
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-15 21:02:19 +0300

    Add theme toggle module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles/rewrite.scss";
 import "./selectors/arithmeticOperationSelector";
 import "./selectors/advancedOperationSelector";
+import "./screen/toggleTheme";
 import "./buttons/numbers";
 import "./screen/updateScreen";
