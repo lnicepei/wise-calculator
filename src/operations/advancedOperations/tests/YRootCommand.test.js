3807a6040440299806cac0ece417924ff6894ce8
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-18 09:30:45 +0300

    Add beforeEach hook

--- src/operations/advancedOperations/tests/YRootCommand.test.js
+++ src/operations/advancedOperations/tests/YRootCommand.test.js
@@ -6,40 +6,34 @@ const { YRootCommand } = require("../YRootCommand");
 
 const { NumberButtonCommand } = require("../../../buttons/numbers");
 
-test.skip("should not change previousValue and value on first execution", () => {
+beforeEach(() => {
   calculator.value = null;
   calculator.previousValue = null;
   calculator.operationSigns = [];
+});
+
+test.skip("should not change previousValue and value on first execution", () => {
   calculator.execute(new YRootCommand());
   expect(calculator.previousValue).toBe(null);
   expect(calculator.value).toBe(null);
 });
 
-test.skip("should be executed with one operand", () => {
+test("should be executed with one operand", () => {
   calculator.previousValue = 4;
-  calculator.value = null;
-  calculator.operationSigns = [];
-
   calculator.execute(new YRootCommand());
-
   expect(calculator.value).toBe(0);
 });
 
 test("should work properly with two operands", () => {
   calculator.previousValue = 3;
   calculator.value = 27;
-  calculator.operationSigns = [];
 
   calculator.execute(new YRootCommand());
 
   expect(calculator.previousValue).toBe(3);
 });
 
-test.skip("should execute previous operation before being executed itself", () => {
-  calculator.previousValue = null;
-  calculator.value = null;
-  // calculator.operationSigns = [];
-
+test("should execute previous operation before being executed itself", () => {
   calculator.execute(new NumberButtonCommand(1));
   calculator.operationSigns.push("+");
   calculator.execute(new NumberButtonCommand(2));
