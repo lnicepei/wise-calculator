307dcfd33ece54459a26b436caae5cb266b0b0bf
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-18 07:49:05 +0300

    Toggle one class of body

--- src/screen/toggleTheme.js
+++ src/screen/toggleTheme.js
@@ -3,11 +3,5 @@ const toggle = document.querySelector(".theme-toggle");
 toggle.addEventListener("click", toggleTheme);
 
 function toggleTheme() {
-  const elementsToToggle = document.querySelectorAll(
-    "body, .advanced-operations, .arithmetic-operations, .arithmetic-operations > div, .equals, .undo, .top-bar, .theme-toggle, .calculator, .number"
-  );
-
-  elementsToToggle.forEach((element) => {
-    element.classList.toggle("lightMode");
-  });
+  document.querySelector("body").classList.toggle("lightMode");
 }
