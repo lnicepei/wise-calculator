633b060826131cab760ec1543ff310536999716f
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-20 10:05:09 +0300

    Add binary more binary operations

--- src/selectors/arithmeticOperationSelector.js
+++ src/selectors/arithmeticOperationSelector.js
@@ -1,4 +1,10 @@
+import { BinaryCommand } from "../calculator/binaryOperations";
 import { calculator } from "../calculator/calculator";
+import { UnaryCommand } from "../calculator/unaryOperations";
+import {
+  PowerOfYCommand,
+  YRootCommand,
+} from "../operations/advancedOperations/index";
 import {
   AddCommand,
   DivideCommand,
@@ -20,51 +26,58 @@ export function arithmeticCommandSelector(event) {
   const operation = event?.target?.textContent || event;
   const previousCommand = calculator.operations.at(-1);
 
-  if (
-    !(
-      previousCommand instanceof AddCommand ||
-      previousCommand instanceof SubtractCommand ||
-      previousCommand instanceof MultiplyCommand ||
-      previousCommand instanceof DivideCommand
-    ) ||
-    operation === "undo" ||
-    operation === "y√x" ||
-    operation === "AC" ||
-    operation === "x^y"
+  // if (
+  //   //   //   // !(
+  //   //   //   //   previousCommand instanceof AddCommand ||
+  //   //   //   //   previousCommand instanceof SubtractCommand ||
+  //   //   //   //   previousCommand instanceof MultiplyCommand ||
+  //   //   //   //   previousCommand instanceof DivideCommand
+  //   //   //   // ) ||
+  //   //   //   // operation === "undo" ||
+  //   //   //   // operation === "y√x" ||
+  //   //   //   // operation === "AC" ||
+  //   //   //   // operation === "x^y"
+  //   (previousCommand instanceof BinaryCommand)
+  // ) {
+  switch (
+    // (operation !== "undo" &&
+    //   calculator.operationSigns.at(-1) !== "=" &&
+    calculator.operationSigns.pop() ||
+    operation
   ) {
-    switch (
-      (operation !== "undo" &&
-        calculator.operationSigns.at(-1) !== "=" &&
-        calculator.operationSigns.at(-1)) ||
-      operation
-    ) {
-      case "+":
-        calculator.execute(new AddCommand());
-        break;
-      case "-":
-        calculator.execute(new SubtractCommand());
-        break;
-      case "*":
-        calculator.execute(new MultiplyCommand());
-        break;
-      case "/":
-        calculator.execute(new DivideCommand());
-        break;
-      case "=":
-        calculator.execute(new EqualsCommand());
-        break;
-      case "undo":
-        calculator.undo();
-        break;
-      default:
-        advancedCommandSelector(calculator.operationSigns.at(-1) || operation);
-        break;
-    }
-
-    if (operation !== "y√x" && operation !== "x^y" && operation !== "undo")
-      calculator.operationSigns.push(operation);
-  } else {
-    calculator.operationSigns.splice(-1, 1, operation);
+    case "+":
+      calculator.execute(new AddCommand());
+      break;
+    case "-":
+      calculator.execute(new SubtractCommand());
+      break;
+    case "*":
+      calculator.execute(new MultiplyCommand());
+      break;
+    case "/":
+      calculator.execute(new DivideCommand());
+      break;
+    case "x^y":
+      calculator.execute(new PowerOfYCommand());
+      break;
+    case "y√x":
+      calculator.execute(new YRootCommand());
+      break;
+    case "=":
+      calculator.execute(new EqualsCommand());
+      break;
+    case "undo":
+      calculator.undo();
+      break;
+    default:
+      advancedCommandSelector(operation);
+      break;
   }
+  // } else {
+  // calculator.operationSigns.splice(-1, 1, operation);
+  // calculator.operationSigns.push(operation);
+  // }
   console.log(calculator);
+  if (operation !== "undo" && operation !== "=")
+    calculator.operationSigns.push(operation);
 }
