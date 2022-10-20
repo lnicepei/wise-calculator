4e2c71ba9558de9873325c1e6d4675825ffa65a0
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-20 10:04:37 +0300

    Move file

---
+++ src/operations/arithmeticOperations/PowerOfYCommand.js
@@ -0,0 +1,18 @@
+import { BinaryCommand } from "../../calculator/binaryOperations";
+import { calculator } from "../../calculator/calculator";
+import { updateScreen } from "../../screen/updateScreen";
+
+export class PowerOfYCommand extends BinaryCommand {
+  constructor() {
+    super();
+  }
+
+  execute() {
+    super.execute();
+    if (this.validate()) {
+      calculator.previousValue = calculator.previousValue ** calculator.value;
+      updateScreen();
+    }
+    calculator.value = 0;
+  }
+}
