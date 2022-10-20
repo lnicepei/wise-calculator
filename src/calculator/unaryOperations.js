36e60f991dd4490a96785ca47b79898f871e5a33
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-20 10:52:53 +0300

    Remove old comments

--- src/calculator/unaryOperations.js
+++ src/calculator/unaryOperations.js
@@ -1,9 +1,5 @@
-// класс бинарных операций
-// и здесь валидировать
-
 import { updateScreen } from "../screen/updateScreen";
 import { arithmeticCommandSelector } from "../selectors/arithmeticOperationSelector";
-import { BinaryCommand } from "./binaryOperations";
 import { calculator, Calculator } from "./calculator";
 
 export class UnaryCommand extends Calculator {
@@ -16,11 +12,10 @@ export class UnaryCommand extends Calculator {
   execute() {
     if (
       calculator.operationSigns.at(-1) !== undefined
-      // calculator.operations.at(-1) instanceof BinaryCommand
+      // && calculator.value !== 0
     )
       arithmeticCommandSelector(calculator.operationSigns.at(-1));
     calculator.value = null;
-    // updateScreen();
   }
 
   undo() {
