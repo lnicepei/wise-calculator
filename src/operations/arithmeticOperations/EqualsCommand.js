568316a7800a59351acd6c11a12e6bc9afe753a2
lnicepei <dmitrii.shaidorov@gmail.com>
2022-10-20 10:54:51 +0300

    Leave only execute method

--- src/operations/arithmeticOperations/EqualsCommand.js
+++ src/operations/arithmeticOperations/EqualsCommand.js
@@ -1,66 +1,13 @@
-import { BinaryCommand } from "../../calculator/binaryOperations";
-import { calculator } from "../../calculator/calculator";
+import { UnaryCommand } from "../../calculator/unaryOperations";
+import { updateScreen } from "../../screen/updateScreen";
 
-export class EqualsCommand extends BinaryCommand {
+export class EqualsCommand extends UnaryCommand {
   constructor() {
     super();
   }
 
-  // executeFirstOperation() {}
-
-  // executeWithOneArg() {
-  // if (
-  //   calculator.operationSigns.at(-1) !== "=" &&
-  //   calculator.operationSigns.at(-1) !== undefined
-  // ) {
-  //   arithmeticCommandSelector(calculator.operationSigns.at(-1));
-  // }
-  // }
-
-  // executeWithTwoArgs() {
-  // if (
-  //   calculator.operationSigns.at(-1) !== "=" &&
-  //   calculator.operationSigns.at(-1) !== undefined
-  // ) {
-  //   arithmeticCommandSelector(calculator.operationSigns.at(-1));
-  //   calculator.value = 0;
-  // } else if (
-  //   calculator.operationSigns.at(-1) !== "=" &&
-  //   calculator.operationSigns.at(-1) === undefined
-  // ) {
-  //   calculator.previousValue = 0;
-  // } else {
-  //   calculator.operationSigns.pop();
-  //   this.executeWithTwoArgs();
-  // }
-  // }
-
   execute() {
-    // if (calculator.value === null) {
-    //   if (
-    //     calculator.operationSigns.at(-1) !== "=" &&
-    //     calculator.operationSigns.at(-1) !== undefined
-    //   ) {
-    //     arithmeticCommandSelector(calculator.operationSigns.at(-1));
-    //   }
-    // } else {
-    //   if (
-    //     calculator.operationSigns.at(-1) !== "=" &&
-    //     calculator.operationSigns.at(-1) !== undefined
-    //   ) {
-    //     arithmeticCommandSelector(calculator.operationSigns.at(-1));
-    //     calculator.value = 0;
-    //   } else if (
-    //     calculator.operationSigns.at(-1) !== "=" &&
-    //     calculator.operationSigns.at(-1) === undefined
-    //   ) {
-    //     calculator.previousValue = 0;
-    //   } else {
-    //     calculator.operationSigns.pop();
-    //     this.execute();
-    //   }
-    // }
     super.execute();
-    calculator.value = null;
+    updateScreen();
   }
 }
